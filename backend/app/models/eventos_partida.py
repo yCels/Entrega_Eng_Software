7130ae75edcfa0b628f37b5bb1e7d67f9a32f450
ycels <celso5mf@gmail>
2026-09-10 10:29:30 -0300

    feat(models) : add enums na coluna tipo na classe Partida

--- backend/app/models/eventos_partida.py
+++ backend/app/models/eventos_partida.py
@@ -1,8 +1,8 @@
 class EventoPartida(Base):
     __tablename__="eventos_partida"
     id=Column(Integer, primary_key=True)
-    tipo=Column(Enum)
+    tipo=Column(Enum("cartao_vermelho","cartao_amarelo","substituicao","gol"))
     minuto=Column(Integer)
     jogador_id=Column(Integer, ForeignKey("jogadores.id"))
     partida_id=Column(Integer , ForeignKey("partidas.id"))
-    time_id=Column(Integer , ForeignKey)
+    time_id=Column(Integer , ForeignKey("times.id"))
